eda5183ecf6f48e6552073c0a818452f58483d86
Michael Gamatero <mgamatero@yahoo.com>
2019-03-26 22:40:03 -0700

    update grid, dynamic background images

--- src/component/App.js
+++ src/component/App.js
@@ -3,7 +3,6 @@ import Dropdown from './Dropdown';
 import Information from './Information';
 import CurrencyConverter from './CurrencyConverter';
 import './App.css';
-// import MapGeocode from './MapGeocode';
 import axios from 'axios';
 
 
@@ -22,28 +21,30 @@ class App extends React.Component {
         selectedCountryCurrencyName: null,
         selectedCountryCurrencySymbol: null,
         dollarsToConvert: 0,
-        selectedCurrencyConversionRate:0,
-        convertedAmount:0
+        selectedCurrencyConversionRate: 0,
+        convertedAmount: 0,
+        selectedCountryBackgroundImage: 'https://drflkq9jqzld5.cloudfront.net/sites/default/files/main/articles/2019/02/21/forex.jpg'
     }
 
     //*********************************************************************** */
-    // This function takes loads JSON info from openaq.org and loads into state
+    // This function takes loads JSON info from openaq.org and loads into state.
+    // It will be used by the Dropdown component
     // *************************************************************************
     JSONloader = () => {
         axios.get('https://api.openaq.org/v1/countries/', {
         }).then((response) => {
-            console.log(response.data.results)
             this.setState({ countryInfo: response.data.results })
-            // console.log(this.state.countryInfo)
+            
         });
     }
 
 
     //********************************************************************** */
     // This is a callback function used by Dropdown component.
-    // 1) It takes a countryCode from the Dropdown 
+    // 1) It takes a countryCode selected from the Dropdown 
     // 2) Uses the countryCode to fetch country data from "restcountries.eu"
     // 3) and sets properties to the state.
+    // 4) Uses helper function BackgroundUpdateFromCountry() - to update background
     //********************************************************************** */
     CountryCodeFromDropDown = (countryCode) => {
         this.setState({ selectedCountryCode: countryCode })
@@ -61,8 +62,11 @@ class App extends React.Component {
                 selectedCountryCurrencySymbol: response.data[0].currencies[0].symbol,
                 selectedCountryLatLng: response.data[0].latlng
             })
+        }).then((response) => {
+            this.BackgroundUpdateFromCountry()
         })
     }
+
 
 
 
@@ -90,15 +94,31 @@ class App extends React.Component {
             }
         })
             .then((data) => {
-                // console.log("converted" + JSON.stringify(data))
-                // console.log(data.data.rates[this.state.selectedCountryCurrency])
+                //get the rate and setState
                 let rate = parseFloat(data.data.rates[this.state.selectedCountryCurrency])
-                this.setState({selectedCurrencyConversionRate: rate})
+                this.setState({ selectedCurrencyConversionRate: rate })
 
+                //calculate converted value and setState
                 let convertedMoney = (rate * parseFloat(this.state.dollarsToConvert)).toFixed(2)
-                this.setState({convertedAmount:convertedMoney})
-                // console.log(`Rate: ${rate} * Dollars: ${this.state.dollarsToConvert} = ${convertedMoney})`)
+                this.setState({ convertedAmount: convertedMoney })
             })
+    }
+
+// ******************************************************************************************
+// This function updates the background dynamically.  Unsplash API is used.
+// ******************************************************************************************
+    BackgroundUpdateFromCountry = () => {
+        axios.get('https://api.unsplash.com/search/photos/', {
+            params: { query: `'${this.state.selectedCountryName}'` },
+            headers:
+            {
+                Authorization: 'Client-ID 5091f4b047af903d52662e847073ebad93e1db88e12030eb0b1ab957725d9086'
+            }
+        }).then((response) => {
+            // console.log("Background Update" + response.data.results[0].urls.full)
+            this.setState({ selectedCountryBackgroundImage: response.data.results[0].urls.regular })
+        })
+
     }
 
 
@@ -120,12 +140,12 @@ class App extends React.Component {
 
 
         return (
-            
-                <div className="container">
+            <div style={{ backgroundColor: `black` }}>
+                <div className="container" style={{ backgroundImage: `url("${this.state.selectedCountryBackgroundImage}")` }} >
                     <div className="row">
                         <div className="col">
                             <div className="align-middle">
-                                <div className="card bg-light m-3">
+                                <div className="card bg-light my-5">
                                     <div className="card-body">
                                         <Dropdown state={this.state} CountryCodeFromDropDown={this.CountryCodeFromDropDown} />
                                     </div>
@@ -134,7 +154,7 @@ class App extends React.Component {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="d-none d-md-block col-md-4 m-3">
+                        <div className="d-none d-md-inline col-md-4 mb-5">
                             <Information
                                 name={this.state.selectedCountryName}
                                 flag={this.state.selectedCountryFlag}
@@ -148,14 +168,14 @@ class App extends React.Component {
                             />
                         </div>
 
-                        <div className="col-md-8 m-3">
+                        <div className="col-md-8 mb-5">
                             <CurrencyConverter symbol={this.state.selectedCountryCurrencySymbol} currencyName={this.state.selectedCountryCurrencyName} rate={this.state.selectedCurrencyConversionRate} converted={this.state.convertedAmount} GetDollarFromCurrencyConverter={this.GetDollarFromCurrencyConverter} />
                         </div>
 
 
                     </div>
                 </div>
-            
+            </div>
         )
     }
 }
